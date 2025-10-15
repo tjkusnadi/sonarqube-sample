63f044460b970556ecef770d9b346b1f8fe2c2e1
Tije Kusnadi <tijekusnadi05@gmail.com>
2025-10-15 19:49:58 +0700

    test: add test

--- sample-js/src/index.js
+++ sample-js/src/index.js
@@ -1,11 +1,7 @@
 const lodash = require('lodash');
 
-console.log('Lodash version:', lodash.VERSION);
-
 function add(a, b) {
     return a + b;
 }
-
-console.log('2 + 3 =', add(2, 3));
 
 module.exports = { add };
